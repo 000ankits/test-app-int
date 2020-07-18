5e1a672d8cb4f0d331a618c78f051fb2af8db524
unknown <ankit.sircar@gmail.comgit config user.emailgit config --global user.name Ankit>
2020-07-18 17:53:29 +0530

    added myposts & create post features

--- server.js
+++ server.js
@@ -14,12 +14,17 @@ const dbConn = new Client({
 		rejectUnauthorized : false
 	}
 });
+dbConn.connect();
 
 app.use(express.static('views'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(session({ secret: 'Password Encryption', resave: false, saveUninitialized: false }));
+app.use((req, res, next) => {
+	res.locals.currentUser = req.session.username;
+	next();
+});
 
 const storage = multer.memoryStorage(),
 	upload = multer({ storage: storage });
@@ -29,18 +34,33 @@ const storage = multer.memoryStorage(),
 // ===================================
 
 app.get('/', (req, res) => {
-	res.render('home');
+	const sql = {
+		text   : `select * from posts where privacy = $1`,
+		values : [ 'public' ]
+	};
+	dbConn
+		.query(sql)
+		.then((data) => {
+			// data.forEach((row) => {
+
+			// });
+			return res.render('home', { posts: data.rows });
+		})
+		.catch((e) => {
+			console.log(e);
+			res.end();
+			// res.redirect('/');
+		});
 });
 
 app.get('/login', (req, res) => {
+	if (req.session.user) {
+		return res.redirect('/message');
+	}
 	res.render('login');
 });
 
 app.post('/login', (req, res) => {
-	if (req.session.user) {
-		req.session.message = 'You are already logged in';
-		res.redirect('/message');
-	}
 	if (!req.body.username || !req.body.pass) {
 		res.redirect('/login');
 	} else {
@@ -48,18 +68,15 @@ app.post('/login', (req, res) => {
 			text   : `select * from users where username = $1 and password = $2`,
 			values : [ req.body.username, req.body.pass ]
 		};
-		dbConn.connect();
 		dbConn
 			.query(sql)
 			.then((data) => {
-				console.log(data);
-				dbConn.end();
+				// console.log(data);
 				req.session.user = req.body.username;
-				res.redirect('/posts');
+				res.redirect('/myposts');
 			})
 			.catch((e) => {
 				console.log(e);
-				dbConn.end();
 				res.redirect('/login');
 			});
 	}
@@ -67,8 +84,7 @@ app.post('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
 	if (req.session.user) {
-		req.session.message = 'You are already registered';
-		res.redirect('/message');
+		return res.redirect('/message');
 	}
 	if (req.session) res.render('register');
 });
@@ -89,32 +105,93 @@ app.post('/register', (req, res) => {
 			text   : `insert into users(username, email, password) values($1, $2, crypt( $3, gen_salt('bf')))`,
 			values : [ req.body.username, req.body.email, req.body.pass ]
 		};
-		dbConn.connect();
 		dbConn
 			.query(sql)
 			.then((data) => {
-				console.log(data);
-				dbConn.end();
-				res.redirect('/login');
+				// console.log(data);
+				return res.redirect('/login');
 			})
 			.catch((e) => {
 				console.log(e);
-				dbConn.end();
-				res.redirect('/register');
+				return res.redirect('/register');
 			});
 	}
 });
 
-app.get('/posts', (req, res) => {
+app.get('/myposts', (req, res) => {
 	if (!req.session.user) {
-		res.redirect('/login');
+		return res.redirect('/login');
 	}
-	res.send('you have posts');
+	const sql = {
+		text   : `select * from posts where username = $1`,
+		values : [ req.session.user ]
+	};
+	dbConn
+		.query(sql)
+		.then((data) => {
+			// data.forEach((row) => {
+
+			// });
+			return res.render('myPosts', { posts: data.rows });
+		})
+		.catch((e) => {
+			console.log(e);
+			res.redirect('/');
+		});
+});
+
+app.get('/createPost', (req, res) => {
+	if (!req.session.user) {
+		return res.redirect('/login');
+	}
+	res.render('createPost');
+});
+
+app.post('/createPost', (req, res) => {
+	if (!req.session.user) {
+		return res.redirect('/login');
+	}
+	if (!req.body.title || !req.body.privacy) {
+		return res.redirect('/createPost');
+	}
+
+	const sql = {
+		text   : `insert into posts (username, title, imglink, description, privacy) values($1, $2, $3, $4, $5)`,
+		values : [ req.session.user, req.body.title, req.body.img, req.body.description, req.body.privacy ]
+	};
+	dbConn.query(sql, (err, result) => {
+		if (err) {
+			console.log(err);
+			return res.redirect('/createPost');
+		} else {
+			console.log('success!');
+			return res.redirect('/myposts');
+		}
+	});
+	// dbConn
+	// 	.query(sql)
+	// 	.then((data) => {
+	// 		console.log('success!');
+	// 		return res.redirect('/myposts');
+	// 	})
+	// 	.catch((e) => {
+	// 		console.log(e);
+	// 		return res.redirect('/');
+	// 	});
+	// res.redirect('/login');
 });
 
 app.get('/message', (req, res) => {
-	res.render('message', { message: req.session.message });
-	req.session.message = '';
+	res.render('message', { message: 'You are already logged in' });
+});
+
+app.get('/logout', (req, res) => {
+	req.session.destroy();
+	res.redirect('/');
+});
+
+dbConn.on('error', (err) => {
+	console.log(err);
 });
 
 const port = process.env.PORT || 8888;
